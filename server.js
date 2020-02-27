7d952f23f71479e269c38c09c5795897be290667
Emanuel caceres <emanuelalfonsoc@gmail.com>
2020-02-27 12:09:32 -0300

    commit2

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var http = require("http");
 
-http.createServer(function(request, response){
+function iniciar(){
+	function onRequest(request, response){
+		console.log("request recibido");
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("holis");
 	response.end();
+	console.log("request terminado");
 
 }
 http.createServer(onRequest).listen(8080);
+console.log("Servidor inicial");
+	}
+	exports.iniciar = iniciar;
